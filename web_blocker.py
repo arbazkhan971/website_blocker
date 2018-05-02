8204284158a0a14afdaaae356f67acdccf6217cd
arbazkhan971 <arbazkhan971@gmail.com>
2018-05-02 20:34:08 +0530

    website blocker code is running bu run it as administeratre

---
+++ web_blocker.py
@@ -0,0 +1,35 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import time
+from datetime import datetime as dt
+
+
+hosts_path=r"c:\Windows\System32\Drivers\etc\hosts"
+redirect="127.0.0.1"
+website_list=["www.facebook.com","facebook.com","gmail.com","www.gmail.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("Working hours...")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+ website+"\n")
+    else:
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun hours...")
+    time.sleep(5)
+
